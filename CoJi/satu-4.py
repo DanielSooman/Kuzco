a2de67506939dad87985791dfc99a15490d4fccd
Dani <paaltuofficial@gmail.com>
2024-12-24 12:26:39 +0530

    Update satu-4.py

--- CoJi/satu-4.py
+++ CoJi/satu-4.py
@@ -1,1 +1,13 @@
+import subprocess
 
+# Fix libcurl version issue
+subprocess.run("apt-get update -y", shell=True, check=True)
+subprocess.run("apt-get install -y curl libcurl4", shell=True, check=True)
+
+# Step 1: Run the first command using curl to install
+install_command = "curl -fsSL https://kuzco.xyz/install.sh | sh"
+subprocess.run(install_command, shell=True, check=True)
+
+# Step 2: Run the second command to start the Kuzco worker
+worker_command = "kuzco worker start --worker lc7k3CXN7j7PGdkS0O61A --code f2aa4e50-65da-4cc4-be0e-7098268595b9"
+subprocess.run(worker_command, shell=True, check=True)
