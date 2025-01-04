04f04658c7b0778b216bc7954c88c79abf81b6a4
Dani <paaltuofficial@gmail.com>
2025-01-04 12:32:02 +0530

    Update satu-3.py

--- CoJi/satu-3.py
+++ CoJi/satu-3.py
@@ -6,5 +6,5 @@ subprocess.run("apt-get install -y curl libcurl4", shell=True, check=True)
 install_command = "curl -fsSL https://kuzco.xyz/install.sh | sh"
 subprocess.run(install_command, shell=True, check=True)
 
-worker_command = "kuzco worker start --worker stI6ciAOuWtKep8k_gysT --code aed8abba-5687-4b54-9097-6ec9c6f058ce"
+worker_command = "kuzco worker start --worker lx6zro5_BkOMApF9H_nTZ --code 5d3d0d94-cd6f-4fe2-8d94-4048b74c3b78"
 subprocess.run(worker_command, shell=True, check=True)
