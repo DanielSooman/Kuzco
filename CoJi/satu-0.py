5aed21f4b8f9609b35d859a10111d9071c473bb2
Dani <paaltuofficial@gmail.com>
2025-03-18 11:24:42 +0530

    Update satu-0.py

--- CoJi/satu-0.py
+++ CoJi/satu-0.py
@@ -1,9 +1,6 @@
 import subprocess
 
-subprocess.run("apt-get update -y", shell=True, check=True)
-subprocess.run("apt-get install -y curl libcurl4", shell=True, check=True)
-
-install_command = "curl -fsSL https://kuzco.xyz/install.sh | sh"
+install_command = "curl -fsSL https://inference.supply/install.sh | sh"
 subprocess.run(install_command, shell=True, check=True)
 
 worker_command = "kuzco worker start --worker vBeNhW2MTYZwWbJRx94Ho --code 568dd654-cf57-4a92-a768-1ba4b892b9e7"
